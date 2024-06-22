56176089f413553f8eff32b4e99a47bb71d8e706
ki1lmyself <sofiamiklzkova@gmail.com>
2024-06-22 17:37:19 +0300

    Добавьте файлы проекта.

---
+++ LabWork5/Book.cs
@@ -0,0 +1,67 @@
+﻿namespace LabWork5
+{
+    class Book
+    {
+        //Task1
+        private string _name;
+        private string _author;
+        private int _price;
+
+        public Book()
+             : this("Неизвестно", "Неизвестно", 0)
+        {
+        }
+
+        public Book(string name, string author, int price)
+        {
+            _name = name;
+            _author = author;
+            _price = price;
+        }
+
+        //Task2
+        public void Print()
+        {
+            Console.WriteLine($"Название: {_name}, Автор: {_author}, Цена: {_price}");
+        }
+
+        //Task3
+        public string GetTitle
+        {
+            get { return _name; }
+            set
+            {
+                if (_name.Length < 0)
+                {
+                    throw new ArgumentException("Название не может быть пустым");
+                }
+            }
+        }
+
+        public string GetAuthor
+        {
+            get { return _author; }
+            set
+            {
+                if (_author.Length < 0)
+                {
+                    throw new ArgumentException("Автор не может быть пустым.");
+                }
+            }
+        }
+
+        public int GetPrice
+        {
+            get { return _price; }
+            set
+            {
+                if (_price < 0)
+                {
+                    throw new ArgumentException("Цена не может быть меньше 0.");
+                }
+            }
+        }
+    }
+}
+
+
---
+++ LabWork5/Program.cs
@@ -0,0 +1,39 @@
+﻿using LabWork5;
+
+class Program
+{
+    static void Main()
+    {
+        //Task1
+        Book book1 = new Book();
+        Book book2 = new Book("Евгений Онегин", "A.C. Пушкин", 568);
+        Book book3 = new Book("Собачье сердце", "М.А. Булгаков", 889);
+        Book book4 = new Book("", "", -1);
+
+        //Task2
+        book1.Print();
+        book2.Print();
+        book3.Print();
+
+        //Task3
+        Console.WriteLine($"Название: {book1.GetTitle}");
+        Console.WriteLine($"Автор: {book2.GetAuthor}");
+        Console.WriteLine($"Цена: {book3.GetPrice}");
+
+        Console.WriteLine($"Название: {book4.GetTitle}");
+        Console.WriteLine($"Автор: {book4.GetAuthor}");
+        Console.WriteLine($"Цена: {book4.GetPrice}");
+
+        try
+        {
+            book4.GetTitle = "";
+            book4.GetAuthor = "";
+            book4.GetPrice = -1;
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
+
